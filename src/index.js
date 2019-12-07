111adf911e23d276e18c2f09adb9b8e5549c4cc5
Teresa Cho <tcho@cca.edu>
2019-12-06 20:14:13 -0800

    changes the value of square with a function that will handle a click

--- src/index.js
+++ src/index.js
@@ -31,7 +31,10 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square 
+      value={this.state.square[i]}
+      onClick={() => this.handleClick[i]} 
+    />;
   }
 
   render() {
