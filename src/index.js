971ebd78e74f14df968fba09566968531a6e6dd1
Teresa Cho <tcho@cca.edu>
2019-12-07 16:36:59 -0800

    game board will now check boolean state to change turns from X to Os

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ class Board extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext : !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
