d4684fa33f4892adfb45bd30d8909cb335ce78b9
Teresa Cho <tcho@cca.edu>
2019-12-07 16:27:36 -0800

    fixes the square vs squares in code

--- src/index.js
+++ src/index.js
@@ -19,15 +19,23 @@ class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      square: Array(9).fill(null),
+      squares: Array(9).fill(null),
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square 
-      value={this.state.square[i]}
-      onClick={() => this.handleClick[i]} 
-    />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick[i]} 
+      />
+    );
   }
 
   render() {
