299f685b3f6c228a8d344b928649c5ea94e86e02
Teresa Cho <tcho@cca.edu>
2019-12-07 17:18:17 -0800

    takes away constructor in board, moved to game component and changes state to props

--- src/index.js
+++ src/index.js
@@ -11,13 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
@@ -34,8 +27,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
