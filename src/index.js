9985ad1015a828da02f8605bdb86a4c167a98f4a
Teresa Cho <tcho@cca.edu>
2019-12-07 17:25:58 -0800

    takes away the render status in board which is now in game compo

--- src/index.js
+++ src/index.js
@@ -34,17 +34,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
