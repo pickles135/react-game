f84f6768d9c47c95a29e4c3c67ad2415ba74fe88
Teresa Cho <tcho@cca.edu>
2019-12-07 16:39:01 -0800

    will change status depending on whose turn

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
