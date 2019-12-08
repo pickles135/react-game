6629511bda1cb5c97532178ac5eec0a9c4b9a161
Teresa Cho <tcho@cca.edu>
2019-12-07 17:23:16 -0800

    now render function in game component will use most recent history to determine + dosplay the games status

--- src/index.js
+++ src/index.js
@@ -76,13 +76,28 @@ class Game extends React.Component {
     };
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length -1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
