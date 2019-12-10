1229195e84c12e3f60bfe9e1dd74d323ba3f1815
Teresa Cho <tcho@cca.edu>
2019-12-09 22:17:37 -0800

    creates a jumpTo function that will allow us to jump to a certain step/move

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
@@ -67,6 +68,13 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
