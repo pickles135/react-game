d3cb3c5b4cfb7406a640b29a91ba1547288e047b
Teresa Cho <tcho@cca.edu>
2019-12-09 20:29:17 -0800

    lifts the board component up to top level component game

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -34,8 +26,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props(i)}
       />
     );
   }
