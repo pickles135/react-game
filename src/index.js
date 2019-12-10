1f11abea31632f6f5d7b2536bdc6922128fefb0c
Teresa Cho <tcho@cca.edu>
2019-12-09 21:26:34 -0800

    creates a button for history to jump to different moves in the the history

--- src/index.js
+++ src/index.js
@@ -75,6 +75,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
